7df23dcd9953f168a4cb5902599876540d45d181
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-15 15:24:18 +0800

    Gave the demo a facelift and updated packages

--- editable-text-example.js
+++ editable-text-example.js
@@ -78,8 +78,9 @@ Posts.attachSchema(Schemas.Post);
 Comments.attachSchema(Schemas.Comment);
 Meteor.users.attachSchema(Schemas.User);
 
+// Config for editable text widget
 EditableText.useTransactions = true;
-EditableText.clientControlsTransactions = true;
+EditableText.maximumImageSize = 200000;
 
 EditableText.registerCallbacks({
   addTimestampToDoc : function(doc) {
@@ -90,6 +91,9 @@ EditableText.registerCallbacks({
 	return _.extend(doc,extraFields);
   }
 });
+
+// Config for transactions
+tx.requireUser = false; // Means a user who is not logged in gets to undo/redo
 
 if (Meteor.isClient) {
 
@@ -122,7 +126,7 @@ if (Meteor.isServer) {
 	Posts.remove({});
 	Comments.remove({});
 	tx.Transactions.remove({});
-	Posts.insert({_id:"abc123",timestamp:Date.now(),title:"Editable post title - delete this title to remove the post",body:'This is the body of the post, written with the <strong>wysiwyg editor</strong>.  It is editable because we wrote {{&gt; editableText collection="posts" field="body" wysiwyg=true}} in the template instead of {{body}}.<br><br>This demo app was written with meteor packages (aldeed:collection2, babrahams:transactions, ian:accounts-ui-bootstrap-3, meteorhacks:fast-render, babrahams:editable-text-wysiwyg-bootstrap-3) and 200 lines of code (<a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.html" target="_blank">html: 45 loc</a>, <a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.js" target="_blank">js: 104 loc</a>, <a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.css" target="_blank">css: 51 loc</a>). It demonstrates some of the uses of the <a href="https://github.com/JackAdams/meteor-editable-text">babrahams:editable-text</a> package.<br><br>See the source at <a href="https://github.com/JackAdams/editable-text-demo">https://github.com/JackAdams/editable-text-demo.</a>',tags:['Drag to reorder','Click to edit']});
+	Posts.insert({_id:"abc123",timestamp:Date.now(),title:"Editable post title - delete this title to remove the post",body:'This is the body of the post, written with the <strong>wysiwyg editor</strong>.  It is editable because we wrote {{&gt; editableText collection="posts" field="body" wysiwyg=true}} in the template instead of {{body}}.<br /><br />This demo app was written with meteor packages (<a href="https://github.com/aldeed/meteor-collection2" target="_blank">aldeed:collection2</a>, <a href="https://github.com/JackAdams/meteor-transactions" target="_blank">babrahams:transactions</a>, <a href="https://github.com/ianmartorell/meteor-accounts-ui-bootstrap-3" target="_blank">ian:accounts-ui-bootstrap-3</a>, <a href="https://github.com/meteorhacks/fast-render/" target="_blank">meteorhacks:fast-render</a>, <a href="https://github.com/JackAdams/meteor-editable-text-wysiwyg-bootstrap-3" target="_blank">babrahams:editable-text-wysiwyg-bootstrap-3</a>, <a href="https://github.com/JackAdams/meteor-editable-list" target="_blank">babrahams:editable-list</a>) and 222 lines of code (<a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.html" target="_blank">html: 46 loc</a>, <a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.js" target="_blank">js: 122 loc</a>, <a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.css" target="_blank">css: 54 loc</a>). It demonstrates some of the uses of the <a href="https://github.com/JackAdams/meteor-editable-text">babrahams:editable-text</a> package.<br /><br />See the source at <a href="https://github.com/JackAdams/editable-text-demo">https://github.com/JackAdams/editable-text-demo.</a>',tags:['Drag to reorder','Click to edit']});
 	Comments.insert({post_id:"abc123",timestamp:Date.now(),text:"To remove a comment, delete the text and press 'Enter'. This is possible because we wrote {{> editableText collection=\"comments\" field=\"text\" textarea=true removeEmpty=true}} instead of {{text}} in the template.",user:"example@example.com"});
 	Comments.insert({post_id:"abc123",timestamp:Date.now(),text:"Sign in with - email:demo@demo.com, password:password - to see the undo/redo stack in action."});
 	Comments.insert({post_id:"abc123",timestamp:Date.now(),text:"All posts will self destruct every 15 minutes."});
