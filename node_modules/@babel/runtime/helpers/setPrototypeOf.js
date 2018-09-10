b7f4e26cd8474f3ce5e2b5c62d81770b1855ad96
Brent Abrahams <brent_abrahams@yahoo.com>
2018-09-10 11:45:46 +0800

    Fixed babel version

--- node_modules/@babel/runtime/helpers/setPrototypeOf.js
+++ node_modules/@babel/runtime/helpers/setPrototypeOf.js
@@ -1,5 +1,7 @@
+var _Object$setPrototypeOf = require("../core-js/object/set-prototype-of");
+
 function _setPrototypeOf(o, p) {
-  module.exports = _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+  module.exports = _setPrototypeOf = _Object$setPrototypeOf || function _setPrototypeOf(o, p) {
     o.__proto__ = p;
     return o;
   };
